3888f31b03ad229509f927ab6b4ef9a7e9aaf26a
noah0young <drnoahyoung@gmail.com>
2024-12-30 11:02:10 -0500

    Website is  mostly done

--- test.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById("testInput");
-var printButton = document.getElementById("printButton");
-var printValue = document.getElementById("printVal");
-function onPrintClick() {
-    console.log("Success");
-    printValue.value = input.value;
-}
-printButton.addEventListener("click", onPrintClick);
